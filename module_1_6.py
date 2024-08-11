b1c2e0b993d0132950c14345830c44e5c0dcef47
OrioMan <sielsan1@gmail.com>
2024-08-11 15:44:40 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,21 @@
+#my_dict = {'Иван': 1990,'Мария': 1985,'Алексей': 2000 }
+#print("Словарь my_dict:")
+#print(my_dict)
+#print("значение для ключа 'Мария':", my_dict.get('Мария'))
+#print("Ключа 'Елена':", my_dict.get('Елена','Не существует'))
+#my_dict.update({'Максим': 1997,'Олег': 1989 })
+#print("Измененный словарь my_dict :")
+#print(my_dict)
+#removed_value = my_dict.pop("Иван")
+#print("Удалено значение по ключу 'Иван':", removed_value)
+#print("Словарь my_dict после удаления:")
+#print(my_dict)
+
+my_set = { 1, 2, 3, 3, 4}
+print(my_set)
+my_set.update({5,6})
+print(my_set)
+print(my_set.remove(3))
+print(my_set)
+
+
